8b6ff19491edccde136da00b08a77231fce1e71d
OnkeTk <onkemetsetikane@gmail.com>
2023-10-19 05:55:11 -0700

    I'm not going anywhere. You can print that wherever you want to. I'm here and I'm a Spur for life

--- 00_printf.c
+++ 00_printf.c
@@ -13,29 +13,33 @@ int _printf(const char *format, ...)
 	int count = 0;
 		const char *p;
 
-		for (*p = format, *p != '\0'; p++)
+		va_start(args, format);
+
+		for (p = format; *p != '\0'; p++)
 		{
 			if (*p == '%')
 			{
 				p++;
 				if (*p == '%')
-					count += write(q, "%", 1);
+					count += write(1, "%", 1);
 				else if (*p == 'c')
-					count += write(1, &va_arg(args, int), 1);
+					count += write(1, (char*)&(va_arg(args, int)), 1);
 				else if (*p == 's')
-					for (char *s = va_arg(args, char*); *s != '\0'; s++)
+				{
+					if (char *s = va_arg(args, char*); char *s != '\0'; s++)
 						count += write(1, s, 1);
+				}
 				else
 				{
 					count += write(1, "%", 1);
-				count += write(1, p, 1);
+					count += write(1, p, 1);
+				}
 			}
-		}
 			else
 			{
 				count += write(1, p, 1);
 			}
-			va_end(args);
-			return (count);
 		}
+		va_end(args);
+		return (count);
 }
--- main.h
+++ main.h
@@ -1,16 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdarg.h>
-<<<<<<< HEAD
-#include <sdtio.h>
-#include <unistd.h>
-
-int _printf(const char *format, ...);
-=======
 #include <stdio.h>
 #include <unistd.h>
 
 int _printf(const char *format, ...);
-
->>>>>>> 0312cdf31dcc30a10a8021792f619ff7420e5522
 #endif
